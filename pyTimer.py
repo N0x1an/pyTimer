63ef7445f37ec812748eac8af8920b5329455c42
N0x1an <rahman.10jishan@gmail.com>
2023-10-19 13:42:47 -0500

    added in progress bar

--- pyTimer.py
+++ pyTimer.py
@@ -5,17 +5,18 @@
 
 import time
 import datetime
+from tqdm import tqdm
 
 def countDown(hours, minutes, second):
     total_seconds = hours * 3600 + minutes * 60 + second
 
-   
-    while total_seconds > 0 :
-        timer = datetime.timedelta(seconds = total_seconds)
-        print(timer, end="\r")
-        time.sleep(1)
-        total_seconds -= 1
-           
+    with tqdm(total = total_seconds, unit = 's', dynamic_ncols=True) as pbar:
+        while total_seconds > 0 :
+            timer = datetime.timedelta(seconds = total_seconds)
+            print(timer, end="\r")
+            time.sleep(1)
+            total_seconds -= 1
+            pbar.update(1)
 
     print("timeover!")
 
